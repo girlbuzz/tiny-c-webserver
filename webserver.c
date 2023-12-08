301513a8692644b7703097234cc51c7f0de68b40
lj4 <136166458+4lj4@users.noreply.github.com>
2023-12-08 18:46:24 +0000

    HTTP header in server code
    932 bytes now :(

--- webserver.c
+++ webserver.c
@@ -55,7 +55,7 @@ void _start(){
 
     /* Bind sock to port */
     int bind_res;
-    asm_bind(bind_res, server_sock_fd, (struct sockaddr*)&server_addr, sizeof(server_addr));
+    asm_bind(bind_res, server_sock_fd, &server_addr, sizeof(server_addr));
 
     /* Listen for connections */
     asm_listen(server_sock_fd, 16);
@@ -65,29 +65,27 @@ void _start(){
         int client_sock_fd;
         asm_accept(client_sock_fd, server_sock_fd);
 
-        /* TODO - use fork to multithread (maybe) */
-
         /* Receive their request */
         int bytes_received;
-        char recvbuf[1024];
+        char recvbuf[1024]; /* Everything past the first 1024 bytes is ignored*/
         asm_recvfrom(bytes_received, client_sock_fd, recvbuf, 1024, 0);
 
         /* Check if GET */
-        if (recvbuf[0] == 'G' && recvbuf[1] == 'E' && recvbuf[2] == 'T') {
+        if (recvbuf[0] == 'G') { /* This is the most minimal way to possibly check, no other method starts with a 'G' so theres no need to check the rest of the chars. */
             /* Parse filename from request */
-            char filename[1024]; /* 1024 should be enough, but 4096 would be 100% enough, as its the path size limit in linux */
-            int fn_iter;
-            for (fn_iter = 5; recvbuf[fn_iter] != ' ' && fn_iter < bytes_received; fn_iter++) {
-                filename[fn_iter - 5] = recvbuf[fn_iter];
+            char filename[1024]; /* No point going bigger since recvbuf has a size of 1024 too */
+            int fn_iter = 5;
+            while(recvbuf[fn_iter] != ' ' && fn_iter < bytes_received){
+                filename[fn_iter - 5] = recvbuf[fn_iter++];
             }
-            filename[fn_iter - 5] = '\0'; // Null-terminate the filename
+            filename[fn_iter - 5] = '\0';
 
             /* Open the file */
             int file_fd;
             asm_open(file_fd, filename, O_RDONLY, 0644);
-            /* Send 404 page if open fails - assuming the 404.html file will always exist*/
+            /* Send 404 page if open fails - assuming the 404 file will always exist*/
             if(file_fd < 0){
-                asm_open(file_fd, "404.html", O_RDONLY, 0644);
+                asm_open(file_fd, "404", O_RDONLY, 0644);
             }
 
             /* Get file size */
@@ -98,17 +96,26 @@ void _start(){
             char* brk_start;
             asm_brk(brk_start, 0);
             char* brk_end;
-            asm_brk(brk_end, brk_start + (stat_res.st_size * sizeof(char)));
+            asm_brk(brk_end, brk_start + (stat_res.st_size * sizeof(char)) + 18);
+
+            /* Write HTTP/1.1 200 OK into heap */
+            const char* ok = "HTTP/1.1 200 OK\n\n";
+            int i = 0;
+            while(i<18){
+                *(brk_start+i) = ok[i++];
+            }
 
             /* Read file into heap */
-            int file_bytes_read_total;
-            asm_read(file_bytes_read_total, file_fd, brk_start, stat_res.st_size);
+            int file_bytes_read;
+            asm_read(file_bytes_read, file_fd, brk_start+17, stat_res.st_size);
             asm_close(file_fd);
 
-            /* Send the file content in heap to the client */
-            int sendto_res;
-            asm_sendto(sendto_res, client_sock_fd, brk_start, file_bytes_read_total, 0);
+            file_bytes_read += 18;
 
+            /* Send the content in heap to the client */
+            int sendto_res;
+            asm_sendto(sendto_res, client_sock_fd, brk_start, file_bytes_read, 0);
+            
             /* Free heap memory */
             asm_brk(brk_end, brk_start);
         }
