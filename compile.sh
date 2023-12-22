c2210c663b0df35e3d4644502dd6b86bf616e025
lj4 <136166458+4lj4@users.noreply.github.com>
2023-12-22 19:53:49 +0000

    make bin dir

--- compile.sh
+++ compile.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-for file in *.c; do
-    filename=$(basename "$file" .c)
-    tcc "$filename.c" -nostdlib -c -vv
-    ld.gold "$filename.o" -o "$filename"
-    strip --strip-all "$filename"
-    objcopy --strip-section-headers "$filename" "$filename"
-    echo -n "Compiled $filename, executable size (bytes): "
-    stat --format=%s $filename
-done
+tcc webserver.c -nostdlib -c -vv -o ./bin/webserver.o
+ld.gold ./bin/webserver.o -o ./bin/webserver
+strip --strip-all ./bin/webserver
+objcopy --strip-section-headers ./bin/webserver ./bin/webserver
+echo -n "Compiled webserver, executable size (bytes): "
+stat --format=%s ./bin/webserver
